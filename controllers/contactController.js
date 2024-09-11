696cea7e7ec39d10a317f8255155c7ffadfd46ba
DuyTM0508 <duytranls.2003@gmail.com>
2024-09-11 18:04:36 +0700

    CRUD contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -10,9 +10,14 @@ const getAllContact = asyncHandler(async (req, res) => {
 
 //! Get single contact
 const getContact = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Get contact from the backend! ${req.params.id}`,
-  });
+  const contact = await Contact.findById(req.params.id);
+
+  if(!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  res.status(200).json(contact);
 });
 
 //! Create a contact
@@ -24,23 +29,48 @@ const createContact = asyncHandler(async (req, res) => {
     throw new Error("All input is required");
   }
 
-  res.status(200).json({
-    message: `Post to the backend! ${name} ${email} ${phone}`,
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
   });
+
+  res.status(200).json(contact);
 });
 
 //! Update a contact
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `update from the backend! ${req.params.id}`,
-  });
+  const contact = await Contact.findById(req.params.id);
+
+  if(!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const updateContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+      runValidators: true,
+    }
+  )
+
+  res.status(200).json(updateContact);
 });
 
 //! Delete a contact
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: `Delete from the backend! ${req.params.id}`,
-  });
+  const contact = await Contact.findById(req.params.id);
+
+  if(!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+
+  const deleteContact = await Contact.findByIdAndDelete(req.params.id);
+
+  res.status(200).json(deleteContact);
 });
 
 module.exports = {
